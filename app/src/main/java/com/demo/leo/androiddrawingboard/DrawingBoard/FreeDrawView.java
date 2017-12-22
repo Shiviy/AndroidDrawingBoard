3134805fe0f0ce5e4b5e04e818b7d8a7f48f0791
leo <wanying92923@gmail.com>
2017-12-22 10:34:12 +0800

    first commit

---
+++ app/src/main/java/com/demo/leo/androiddrawingboard/DrawingBoard/FreeDrawView.java
@@ -0,0 +1,25 @@
+package com.demo.leo.androiddrawingboard.DrawingBoard;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * 自定义画板
+ * Created by leo on 2017/12/22.
+ */
+
+public class FreeDrawView extends View {
+    public FreeDrawView(Context context) {
+        super(context);
+    }
+
+    public FreeDrawView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FreeDrawView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
